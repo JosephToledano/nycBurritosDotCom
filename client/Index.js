24a8287bf784484d0d338ce6d4bfcdde366ea027
JosephToledano <joseph.a.toledano@gmail.com>
2021-02-01 23:41:01 -0500

    committing to deploy

--- client/Index.js
+++ client/Index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { render } from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { render } from "react-dom";
 
-import App from './App.js';
-import PageRouter from './PageRouter'
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import App from "./App.js";
+import PageRouter from "./PageRouter.js";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 // uncomment so that webpack can bundle styles
 // import styles from './scss/Index.css';
 
-render((
-    <BrowserRouter>
-        <PageRouter/>
-    </BrowserRouter>
-), document.getElementById('root'));
+render(
+  <BrowserRouter>
+    <PageRouter />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
