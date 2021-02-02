a368924eb30bdd44b36366d9de8a6c71943c89de
JosephToledano <joseph.a.toledano@gmail.com>
2021-02-01 23:39:29 -0500

    committing to deploy

--- client/PageRouter.js
+++ client/PageRouter.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './index.css';
-import App from './App.js';
-import Restaurants from './pages/Restaurants';
-import Neighborhoods from './pages/Neighborhoods';
-import Blog from './pages/Blog';
-import About from './pages/About';
-
-
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import "./index.css";
+import App from "./App.js";
+import Restaurants from "./pages/Restaurants.js";
+import Neighborhoods from "./pages/Neighborhoods.js";
+import Blog from "./pages/Blog.js";
+import About from "./pages/About.js";
 
 class PageRouter extends Component {
   render() {
     const PageRouter = () => (
       <div>
         <Switch>
-          <Route exact path='/' component={App}/>
-          <Route path='/Restaurants' component={Restaurants}/>
-          <Route path='/Neighborhoods' component={Neighborhoods}/>
-          <Route path='/Blog' component={Blog}/>
-          <Route path='/About' component={About}/>
+          <Route exact path="/" component={App} />
+          <Route path="/Restaurants" component={Restaurants} />
+          <Route path="/Neighborhoods" component={Neighborhoods} />
+          <Route path="/Blog" component={Blog} />
+          <Route path="/About" component={About} />
         </Switch>
       </div>
-    )
+    );
     return (
       <Switch>
-        <PageRouter/>
+        <PageRouter />
       </Switch>
     );
   }
