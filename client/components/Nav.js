7f784d54107c79a6a37dfc9cba13dc150e78879e
JosephToledano <joseph.a.toledano@gmail.com>
2021-02-02 00:18:34 -0500

    trying again to with routing

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,34 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = (props) => {
-    return (
+  return (
     <nav className="NavBar">
-      <div className="main-header"> 
-      <Link to={'/'}>
+      <div className="main-header">
+        {/* <Link to={'/'}>
                     <h3 className ="NavLink">
                         Home 
                     </h3>
-            </Link>       
-        
-        <Link to={'/'}>
-        <img id= "main-logo" src="https://cdn4.iconfinder.com/data/icons/fast-food-menu-1/64/147_fast-food-menu-roll-burrito-512.png"/>
-        </Link>  
-        <div className ="headerLinks">
-        <Link to={'./About'}>
-        <h3 className ="NavLink">
-            About 
-        </h3>
-      </Link>
-      <Link to={'./Blog'}>
+            </Link>       */}
+
+        {/* <Link to={'/'}> */}
+        <img
+          id="main-logo"
+          src="https://cdn4.iconfinder.com/data/icons/fast-food-menu-1/64/147_fast-food-menu-roll-burrito-512.png"
+        />
+        {/* </Link>   */}
+        <div className="headerLinks">
+          {/* <Link to={'./About'}> */}
+          <h3 className="NavLink">About</h3>
+          {/* </Link> */}
+          {/* <Link to={'./Blog'}>
                 <h3 className ="NavLink">
                     Blog 
                 </h3>
-      </Link>
-      </div>
+      </Link> */}
+        </div>
       </div>
     </nav>
-    )
-}
+  );
+};
 
 export default Nav;
