e1f82fd715de238f5d14499a89c96b6b8c294055
JosephToledano <joseph.a.toledano@gmail.com>
2021-02-02 00:15:06 -0500

    trying again to with routing

--- client/Index.js
+++ client/Index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import { render } from "react-dom";
 
 import App from "./App.js";
-import PageRouter from "./PageRouter.js";
+// import PageRouter from "./PageRouter.js";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 
 // uncomment so that webpack can bundle styles
 // import styles from './scss/Index.css';
 ReactDOM.render(
-  <BrowserRouter>
-    <PageRouter />
-  </BrowserRouter>,
-  //   <App />,
+  //   <BrowserRouter>
+  //     <PageRouter />
+  //   </BrowserRouter>,
+  <App />,
   document.getElementById("root")
 );
